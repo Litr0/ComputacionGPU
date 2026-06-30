bc009b6f1756f64fcc24f1c96b93239a4b28e003
Litr0 <gonzalo.sobarzoa@gmail.com>
2026-06-30 19:50:27 -0400

    Se borran implementaciones que no se van a usar

--- T3/implementations/CudaGol/cudaSharedMemGol.h
+++
@@ -1,5 +0,0 @@
-#include "../common.h"
-
-struct BenchmarkResult computeIterationCudaShared(ubyte*& data, ubyte*& resultData, const uint width, const uint height, const uint iterations, const int blockSize);
-
-void computeIterationCudaSharedWithHalo(ubyte*& data, ubyte*& resultData, const uint width, const uint height, const uint iterations);
